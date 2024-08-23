cbd942b1c285ef3dbcbc288b4d22ffd980bcad80
deji-dd <dejib50@gmail.com>
2024-08-23 21:15:51 +0100

    added signout functionality

--- back/routes/dashboard.js
+++ back/routes/dashboard.js
@@ -6,4 +6,13 @@ router.get("/dashboard", (req, res) => {
   res.render("dashboard");
 });
 
+router.post("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
 export default router;
